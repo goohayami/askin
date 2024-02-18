5254b25073c2d695caf379c662dc49e164af2c82
hayami <goohayami@gmail.com>
2024-02-18 13:41:33 +0900

    add

--- app/toneyama/page.js
+++ app/toneyama/page.js
@@ -33,6 +33,18 @@ export default function Toneyama() {
         </li>
         <li>
           <a className="shop-name">
+            アパマンショップ<span>(56000円～)</span>
+          </a>
+          <br />
+          <Link
+            className="a-link"
+            href="https://www.apamanshop.com/osaka/203/b52019801441128/?media_nm=campus"
+          >
+            レオパレス待兼キャンパス/大阪府豊中市/大阪モノレール柴原阪大前駅
+          </Link>
+        </li>
+        <li>
+          <a className="shop-name">
             UR賃貸住宅 <span>(78,400円～146,000円)</span>
           </a>
           <br />

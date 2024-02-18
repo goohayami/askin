f9912162f2974f79cd7c635e115cf1a3958a0c7e
hayami <goohayami@gmail.com>
2024-02-18 13:40:28 +0900

    commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <div className="main">
         <h1>阪大アパートリスト</h1>
       </div>
+      <ul className="apart-link">
+        <li>
+          <a className="shop-name">
+            アパマンショップ<span>(56000円～)</span>
+          </a>
+          <br />
+          <Link
+            className="a-link"
+            href="https://www.apamanshop.com/osaka/203/b52019801441128/?media_nm=campus"
+          >
+            レオパレス待兼キャンパス/大阪府豊中市/大阪モノレール柴原阪大前駅
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
